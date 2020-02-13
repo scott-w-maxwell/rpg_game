d60026a46944d26a4e91ed59ed16268b086dbcbb
scottwmwork <scottwmwork@users.noreply.github.com>
2020-02-13 14:40:44 -0700

    added moving functionality & changed background

--- game.py
+++ game.py
@@ -1,23 +1,22 @@
 import pygame
 pygame.init()
-white = (255,255,255)
+background = (123, 196, 88)
 screen = pygame.display.set_mode(size=(400,400))
-
+pygame.display.set_caption("First Game")
 # Used for character
 positionY = 200
 positionX =  200
 movement = 10
 
 
-image = pygame.image.load(r'guy.png')
+image = pygame.image.load(r'assets/guy.png')
 image = pygame.transform.scale(image, (100,150))
-screen.fill(white)
+screen.fill(background)
 screen.blit(image, (200,200))
-
-pygame.mixer.music.load("assets/Black Parade.mp3")
+pygame.key.set_repeat(10,100)
+pygame.mixer.music.load("assets/Black Parade.mp3") #TODO Create your own music
 pygame.mixer.music.play()
 while True:
-
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -25,24 +24,25 @@ while True:
 			quit()
 
 		keys = pygame.key.get_pressed()
-
+		
 		if keys[pygame.K_w]:
-			
+		
 			positionY -= movement
-			screen.fill(white)
+			screen.fill(background)
 			screen.blit(image, (positionX, positionY))
 
 		if keys[pygame.K_a]:
+		
 			positionX -= movement
-			screen.fill(white)
+			screen.fill(background)
 			screen.blit(image, (positionX, positionY))
 		if keys[pygame.K_s]:
 			positionY += movement
-			screen.fill(white)
+			screen.fill(background)
 			screen.blit(image, (positionX, positionY))
 		if keys[pygame.K_d]:
 			positionX += movement
-			screen.fill(white)
+			screen.fill(background)
 			screen.blit(image, (positionX, positionY))
 			
 		
