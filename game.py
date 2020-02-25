85a5d699ad8d80db1d668c8acbaa37d020a9ba02
scottwmwork <scottwmwork@gmail.com>
2020-02-25 01:09:20 -0700

    added direction of character based on movement

--- game.py
+++ game.py
@@ -9,18 +9,22 @@ positionX =  200
 movement = 10
 
 # option of character TODO add girl.png
-character = input("guy or girl:")
+# character = input("guy or girl:")
 
-# Load Character sprite
-image = pygame.image.load(f'assets/{character}.png')
-image = pygame.transform.scale(image, (100,150))
 
+# Load Guy Character
+guy_right = pygame.image.load(r'assets/guy_right.png')
+guy_left = pygame.image.load(r'assets/guy_left.png')
+guy_right = pygame.transform.scale(guy_right, (100,150))
+guy_left = pygame.transform.scale(guy_left, (100,150))
+current = guy_right
 
 screen.fill(background)
-screen.blit(image, (200,200))
+screen.blit(guy_right, (200,200))
 pygame.key.set_repeat(10,100)
 pygame.mixer.music.load("assets/Black Parade.mp3") #TODO Create your own music
 pygame.mixer.music.play()
+
 while True:
 
 	for event in pygame.event.get():
@@ -34,21 +38,24 @@ while True:
 		
 			positionY -= movement
 			screen.fill(background)
-			screen.blit(image, (positionX, positionY))
+			screen.blit(current, (positionX, positionY))
 
 		if keys[pygame.K_a]:
 		
 			positionX -= movement
 			screen.fill(background)
-			screen.blit(image, (positionX, positionY))
+			screen.blit(guy_left, (positionX, positionY))
+			current = guy_left
+
 		if keys[pygame.K_s]:
 			positionY += movement
 			screen.fill(background)
-			screen.blit(image, (positionX, positionY))
+			screen.blit(current, (positionX, positionY))
 		if keys[pygame.K_d]:
 			positionX += movement
 			screen.fill(background)
-			screen.blit(image, (positionX, positionY))
+			screen.blit(guy_right, (positionX, positionY))
+			current = guy_right
 			
 		
 		pygame.display.update()
