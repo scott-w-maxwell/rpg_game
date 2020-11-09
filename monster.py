81e9f9ab6b3da3afe1c0f2ecd584fe55b6728ca3
Scott Maxwell <scottwillardmaxwell@protonmail.com>
2020-11-08 20:23:06 -0700

    added functions to player class

--- monster.py
+++ monster.py
@@ -19,7 +19,22 @@ class Monster:
 
 
 class Player(Monster):
-	pass
+
+	def __init__(self, health = 100, attack_lvl = 20, weapon = None):
+		self.health = health
+		self.weapon = weapon
+
+		# Increase attack if player has weapon
+		if weapon:
+			self.attack_lvl = attack_lvl + weapons[weapon]
+		else:
+			self.attack_lvl = attack_lvl
+
+	def attack(self, other_monster):
+		other_monster.health -= self.attack_lvl
+
+	def __str__(self):
+		return f'Health {self.health}\nAttack Lvl: {self.attack_lvl}\nWeapon: {self.weapon}'
 
 
 
